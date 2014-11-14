15f27dee73673ec3dc0d94a55896d16c6974812c
Daniil Mirylenka <dmirylenka@disi.unitn.eu>
2014-11-14 14:39:29 +0100

    Committing the pending 2-year-old changes. Don't remember anymore what they were about.

---
+++ dxtr-api/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to dxtr-api
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ dxtr-api/project.clj
@@ -0,0 +1,11 @@
+(defproject unitn/dxtr-api "0.0.1"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clj-http "0.7.0"]
+                 [org.clojure/data.xml "0.0.7"]
+                 [unitn/wiki-api "0.0.1"]
+                 [unitn/utils "0.0.1"]]
+  :main dxtr-api.core)
